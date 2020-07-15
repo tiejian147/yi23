9818e2334dc0f8cee0a28abc1a2e9b6f25dd08c6
刘生超 <shengchao.liu@yi23.net>
2020-07-15 18:40:54 +0800

    homework02

--- javase/Homework/src/day23/MyStack.java
+++ javase/Homework/src/day23/MyStack.java
@@ -7,6 +7,7 @@ package day23;
 *           2、在栈中提供push方法模拟压栈（栈满了，要有提示信息）
 *           3、在栈中提供pop方法模拟弹栈（栈空了，也要有提示信息）
 *           4、编写测试程序，new栈对象，调用push，pop方法来模拟压栈弹栈的动作
+*           5、假设栈的默认初始化容量是10（请注意无参数构造方法的编写方式）
 *
 *       public class MyStack{//栈类
 *           //提供一个数组来存储栈中的元素
@@ -26,5 +27,33 @@ package day23;
 *       }
 * */
 public class MyStack {
+//    向栈当中存储元素，我们这里使用一维数组模拟，存到栈中，就表示存储到数组中
+//    因为数组是我们学习java的第一个容器
+//    为什么选择Object类型数组？因为这个栈可以存储java中的任何引用类型的数据；
+//    new Animal()对象可以放进去，new Person()对象也可以放进去，因为Animal和Person的超级
+//    父类就是Object；
+//    包括String也可以存储进去，因为String父类也是Object
+    private Object[] elements;
 
+
+//    setter & getter 方法
+
+    public Object[] getElements() {
+        return elements;
+    }
+
+    public void setElements(Object[] elements) {
+        this.elements = elements;
+    }
+
+//    无参构造方法:默认初始化栈容量10；
+
+    public MyStack() {
+        this.elements = new Object[10];
+    }
+
+//    有参构造方法
+    public MyStack(Object[] elements) {
+        this.elements = elements;
+    }
 }
