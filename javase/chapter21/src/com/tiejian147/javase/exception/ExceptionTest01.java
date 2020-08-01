bbb80dd3631ef99f20afaf7cfa9e87fdc7d962e9
刘生超 <shengchao.liu@yi23.net>
2020-08-01 11:58:53 +0800

    ExceptionTest01

---
+++ javase/chapter21/src/com/tiejian147/javase/exception/ExceptionTest01.java
@@ -0,0 +1,10 @@
+package com.tiejian147.javase.exception;
+
+public class ExceptionTest01 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 10;
+        int c = a / b;
+        System.out.println(a + "/" + b + "=" + c);
+    }
+}
