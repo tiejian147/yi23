c10f0263a505a49af1bd6e2d4387c3b40bd867d2
tiejian147 <tiejian147@163.com>
2021-06-26 22:25:25 +0800

    0626test02

--- javase/chapter20/com/tiejian147/javase/string/RviviewStringTest01.java
+++ javase/chapter20/com/tiejian147/javase/string/RviviewStringTest01.java
@@ -1,4 +1,11 @@
 package javase.chapter20.com.tiejian147.javase.string;
 
 public class RviviewStringTest01 {
+    public static void main(String[] args) {
+        System.out.println("111");
+
+        String s1 = "hello world";
+
+        String s2= "zheli sdfsdfd";
+    }
 }
