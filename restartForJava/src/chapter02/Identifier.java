422affa6fb718bcbce51be0506623f16f6a5ac53
tiejian147 <tiejian147@163.com>
2021-07-04 23:59:15 +0800

    'identifier标识符'

---
+++ restartForJava/src/chapter02/Identifier.java
@@ -0,0 +1,16 @@
+package chapter02;
+//标识符：凡是程序员自己能命名的单词就都是标识符；
+//可以标识：类名、方法名、接口名、变量名....
+//main是属于一个方法的名称，但是这个方法名称不能改；因为这个main是SUN公司固定死的；
+/*
+5、标识符是有命名规则的：
+   规则1、标识符只能由数字、字母、下划线、美元符号$组成，不能含有其他符号；
+   规则2、标识符不能以数字开头；
+   视频地址：https://www.bilibili.com/video/BV1ED4y1R7L7?p=55&spm_id_from=pageDriver
+   时间：07:39
+ */
+public class Identifier {
+    public static void main(String[] args) {
+
+    }
+}
