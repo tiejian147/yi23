ccf589f8c5822800af2771b75ceef493e817a2af
tiejian147 <tiejian147@163.com>
2021-07-04 22:23:39 +0800

    'charpter01Summary'

---
+++ restartForJava/src/charpter01/Summary.java
@@ -0,0 +1,9 @@
+package charpter01;
+/*总结：
+1、一个java文件中可以写多个class，public可以不写，如果有的话，只能写一个；
+   并且public修饰的class必须与java文件名称一致；
+
+   视频连接：https://www.bilibili.com/video/BV1ED4y1R7L7?p=53&spm_id_from=pageDriver
+ */
+public class Summary {
+}
