4d8ef8d3e415920a93f013e222348332a226ac7a
tiejian147 <tiejian147@163.com>
2021-07-06 21:27:34 +0800

    'homeworkForVar'

---
+++ restartForJava/src/chapter03/HomeworkForVar.java
@@ -0,0 +1,26 @@
+package chapter03;
+
+public class HomeworkForVar {
+    public static void main(String[] args) {
+//        定义学生的学号
+        int studentNumber = 110;
+        double high = 1.85;
+        String name = "张三";
+        char sex = '男';
+        System.out.println("学号：" + studentNumber);
+        System.out.println("姓名：" + name);
+        System.out.println("性别：" + sex);
+        System.out.println("身高：" + high + "米");
+        System.out.println("-----------------");
+
+//        定义第二个学生的学号
+        int studentNumber2 = 120;
+        double high2 = 1.65;
+        System.out.println("学号：" + studentNumber2);
+        System.out.println("姓名：李四");
+        System.out.println("性别：" + '女');
+        System.out.println("身高：" + high2 + "米");
+
+
+    }
+}
