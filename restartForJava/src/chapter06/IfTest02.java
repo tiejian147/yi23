920e429c1db8023b1c33f36590236b2a30fd6a82
“tiejian147” <tiejian147@163.com>
2021-07-08 10:29:03 +0800

    '070803'

---
+++ restartForJava/src/chapter06/IfTest02.java
@@ -0,0 +1,9 @@
+package restartForJava.src.chapter06;
+
+public class IfTest02 {
+//    测试提交代码
+    public static void main(String[] args) {
+        String s1 = "sdfsd";
+        System.out.println(s1);
+    }
+}
