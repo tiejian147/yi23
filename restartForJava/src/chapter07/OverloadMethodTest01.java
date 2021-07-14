f6b0c51a527a223834b3559b2e2a185cabdb9567
tiejian147 <tiejian147@163.com>
2021-07-15 00:14:59 +0800

    'overloadMethodTest'

---
+++ restartForJava/src/chapter07/OverloadMethodTest01.java
@@ -0,0 +1,46 @@
+package chapter07;
+/*
+自己写一个重载的方法
+ */
+public class OverloadMethodTest01 {
+//    输出换行的方法
+    public static void p(){
+        System.out.println();
+    }
+//    输出byte
+    public static void p(byte b){
+        System.out.println(b);
+    }
+//    输出short
+    public static void p(short b){
+        System.out.println(b);
+    }
+//    输出long
+    public static void p(long b){
+        System.out.println(b);
+    }
+//    输出int
+    public static void p(int b){
+        System.out.println(b);
+    }
+//    输出float
+    public static void p(float b){
+        System.out.println(b);
+    }
+//    输出double
+    public static void p(double b){
+        System.out.println(b);
+    }
+//     输出boolean
+    public static void p(boolean b){
+        System.out.println(b);
+    }
+//    输出char
+    public static void p(char b){
+        System.out.println(b);
+    }
+//    输出String
+    public static void p(String b){
+        System.out.println(b);
+    }
+}
---
+++ restartForJava/src/chapter07/TestNg01.java
@@ -0,0 +1,10 @@
+package chapter07;
+/*
+这就实现了方法重载
+调用了OverloadMethodTest01类中的p方法
+ */
+public class TestNg01 {
+    public static void main(String[] args) {
+        OverloadMethodTest01.p("hello world");
+    }
+}
