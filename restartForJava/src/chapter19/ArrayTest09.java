16dbacfe46cf483d0c16a72d1d3607c12d0b74b7
tiejian147 <tiejian147@163.com>
2021-09-15 22:55:41 +0800

    '二维数组01'

---
+++ restartForJava/src/chapter19/ArrayTest09.java
@@ -0,0 +1,16 @@
+package chapter19;
+/*
+二维数组：
+   就是特殊的一维数组；
+ */
+public class ArrayTest09 {
+    public static void main(String[] args) {
+//        二维数组静态初始化
+        int[][] array = {
+                {100,200,205},
+                {123,145,156,789},
+                {000,012,147,258,3333},
+                {0}
+        };
+    }
+}
