a31e54e41c33ca7de2a3cb480ee5b90f5cb42631
tiejian147 <tiejian147@163.com>
2021-12-14 22:59:31 +0800

    三种类型互转

--- restartForJava/src/chapter20/IntegerTest01.java
+++ restartForJava/src/chapter20/IntegerTest01.java
@@ -33,5 +33,75 @@ public class IntegerTest01 {
         System.out.println("byte的最大值：" + Byte.MAX_VALUE);
         System.out.println("byte的最小值：" + Byte.MIN_VALUE);
 
+        Integer x = 900;
+        System.out.println(x);
+
+        int y = 1000;
+        System.out.println(y);
+
+        Integer z = 800;
+        // +号两边要求是基本数据类型的数字，z是系统做了自动拆箱，所以没有报错；
+        System.out.println(z + 1);
+
+        // 比较两个对象，内存地址肯定是false，装箱的数字比较的也是内存地址
+        Integer a = 1000;
+        Integer b = 1000;
+
+        System.out.println(a == b);//false
+        System.out.println(a.equals(b));//比较两个对象，用equals的话，就可以是true了；
+
+        //Integer的方法
+        //这个静态方法是可以将String类型的数字转化成int类型的数字
+        int intValue = Integer.parseInt("123");
+        System.out.println(intValue + 100);//打印出来的是int 类型
+
+        //String和int互转
+        // int 转String
+        int i4= 33;
+        String s4 = String.valueOf(i4);
+        System.out.println(s4);//这个就是String类型的了
+
+        System.out.println("====");
+        String s6 = i4 + "";
+        System.out.println(s6 + 1);
+
+        //String 转int
+        String s5 = "123";
+        int i5 = Integer.valueOf(s5);
+        System.out.println(i5);//打印结果就是int类型
+
+        int i15 = Integer.parseInt(s5);
+        System.out.println(i15);
+
+
+        //int 和Integer互转
+        // int 转Integer
+        //自动装箱
+        Integer i6 = 444;
+        System.out.println(i6);
+        //调用方法
+        Integer i7 =Integer.valueOf(444);
+        System.out.println(i7);
+
+        //Integer转int
+        //自动拆箱
+        int i8 = i7.intValue();
+        System.out.println(i8);
+
+        //String和Integer互转
+        //String转Integer
+        Integer i9 = Integer.valueOf("123");
+        System.out.println(i9);
+
+        //Integer转String
+        String s10 =String.valueOf(i9);
+        System.out.println(s10);
+
+
+
+
+
+
+
     }
 }
